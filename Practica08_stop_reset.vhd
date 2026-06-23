3a632a9ae4f09307369c7e41523fb289c750af97
viniciodiazav <lvdiazav@gmail.com>
2026-06-22 20:55:18 -0600

    .

--- Practica08_stop_reset.vhd
+++ Practica08_stop_reset.vhd
@@ -1,11 +1,11 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
-USE IEEE.NUMERIC_STD.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY sr IS
 PORT (
-	CLK : IN STD_LOGIC;
+	CLK, RT : IN STD_LOGIC;
 	D7 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
 	X : IN STD_LOGIC_VECTOR(1 DOWNTO 0)
 );
@@ -13,7 +13,7 @@ END sr;
 
 ARCHITECTURE ecu OF sr IS
 
-	SIGNAL CLKC : STD_LOGIC_VECTOR(27 DOWNTO 0)
+	SIGNAL CLKC : STD_LOGIC_VECTOR(27 DOWNTO 0);
 
 	TYPE TYPES IS (
 		R,
@@ -39,72 +39,72 @@ BEGIN
 
 	END PROCESS;
 	
-	PROCESS(ST, X)
+	PROCESS(ST, X, RT)
 	BEGIN
 	
 		CASE ST IS
 		
 			WHEN R =>
-				IF X = "00" AND R = '1' THEN NST <= N0;
-				ELSIF X = "01" AND R = '1' THEN NST <= C;
+				IF X = "00" AND RT = '1' THEN NST <= N0;
+				ELSIF X = "01" AND RT = '1' THEN NST <= C;
 				ELSE NST <= R;
 				END IF;
 		
 			WHEN N0 =>
-				IF X = "00" AND R = '1' THEN NST <= N3;
-				ELSIF X = "01" AND R = '1' THEN NST <= C;
-				ELSIF X = "10" AND R = '1' THEN NST <= N0;
-				ELSE NS <= R;
+				IF X = "00" AND RT = '1' THEN NST <= N3;
+				ELSIF X = "01" AND RT = '1' THEN NST <= C;
+				ELSIF X = "10" AND RT = '1' THEN NST <= N0;
+				ELSE NST <= R;
 				END IF;
 			
 		WHEN N3 =>
-				IF X = "00" AND R = '1' THEN NST <= N7;
-				ELSIF X = "01" AND R = '1' THEN NST <= C;
-				ELSIF X = "10" AND R = '1' THEN NST <= N3;
-				ELSE NS <= R;
+				IF X = "00" AND RT = '1' THEN NST <= N7;
+				ELSIF X = "01" AND RT = '1' THEN NST <= C;
+				ELSIF X = "10" AND RT = '1' THEN NST <= N3;
+				ELSE NST <= R;
 				END IF;
 				
 			WHEN N7 =>
-				IF X = "00" AND R = '1' THEN NST <= N0;
-				ELSIF X = "01" AND R = '1' THEN NST <= C;
-				ELSIF X = "10" AND R = '1' THEN NST <= N7;
-				ELSE NS <= R;
+				IF X = "00" AND RT = '1' THEN NST <= N0;
+				ELSIF X = "01" AND RT = '1' THEN NST <= C;
+				ELSIF X = "10" AND RT = '1' THEN NST <= N7;
+				ELSE NST <= R;
 				END IF;
 			
 			WHEN C =>
-				IF X = "01" AND R = '1' THEN NST <= O;
-				ELSIF X = "00" AND R = '1' THEN NST <= N0;
-				ELSIF X = "11" AND R = '1' THEN NST <= C;
-				ELSE NS <= R;
+				IF X = "01" AND RT = '1' THEN NST <= O;
+				ELSIF X = "00" AND RT = '1' THEN NST <= N0;
+				ELSIF X = "11" AND RT = '1' THEN NST <= C;
+				ELSE NST <= R;
 				END IF;
 			
 			WHEN O =>
-				IF X = "01" AND R = '1' THEN NST <= P;
-				ELSIF X = "00" AND R = '1' THEN NST <= N0;
-				ELSIF X = "11" AND R = '1' THEN NST <= O;
-				ELSE NS <= R;
+				IF X = "01" AND RT = '1' THEN NST <= P;
+				ELSIF X = "00" AND RT = '1' THEN NST <= N0;
+				ELSIF X = "11" AND RT = '1' THEN NST <= O;
+				ELSE NST <= R;
 				END IF;
 				
 			WHEN P =>
-				IF X = "01" AND R = '1' THEN NST <= I;
-				ELSIF X = "00" AND R = '1' THEN NST <= N0;
-				ELSIF X = "11" AND R = '1' THEN NST <= P;
-				ELSE NS <= R;
+				IF X = "01" AND RT = '1' THEN NST <= I;
+				ELSIF X = "00" AND RT = '1' THEN NST <= N0;
+				ELSIF X = "11" AND RT = '1' THEN NST <= P;
+				ELSE NST <= R;
 				END IF;
 			
 			WHEN I =>
-				IF X = "01" AND R = '1' THEN NST <= A;
-				ELSIF X = "00" AND R = '1' THEN NST <= N0;
-				ELSIF X = "11" AND R = '1' THEN NST <= I;
-				ELSE NS <= R;
+				IF X = "01" AND RT = '1' THEN NST <= A;
+				ELSIF X = "00" AND RT = '1' THEN NST <= N0;
+				ELSIF X = "11" AND RT = '1' THEN NST <= I;
+				ELSE NST <= R;
 				END IF;
 			
 			
 			WHEN A =>
-				IF X = "01" AND R = '1' THEN NST <= C;
-				ELSIF X = "00" AND R = '1' THEN NST <= N0;
-				ELSIF X = "11" AND R = '1' THEN NST <= A;
-				ELSE NS <= R;
+				IF X = "01" AND RT = '1' THEN NST <= C;
+				ELSIF X = "00" AND RT = '1' THEN NST <= N0;
+				ELSIF X = "11" AND RT = '1' THEN NST <= A;
+				ELSE NST <= R;
 				END IF;
 		
 		END CASE;
